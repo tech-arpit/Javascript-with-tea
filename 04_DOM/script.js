3a39ffa3eb824d4fe9bc36d10202fe201babc5ef
Arpit <arpit.tiwari2403@gmail.com>
2026-01-19 10:24:39 +0530

    Dom counter practice

--- 04_DOM/script.js
+++ 04_DOM/script.js
@@ -1,29 +1,24 @@
-// DOM ---->>>> Document Object Model 
-// The Document Object Model (DOM) is a programming interface for web documents. It represents the structure of a document as a tree of objects, 
-// allowing programming languages like JavaScript to interact with and manipulate the content, structure, and style of web pages dynamically.
-// IN SIMPLE TERMS --- Frontend developers use DOM to access and change HTML elements, attributes, and styles on a webpage using JavaScript.
-// (frontend ki js..)
 
-// 4 pillars of DOM:--------------->>>>>>>>
-// 1. Selection of an Element 
-// 2. Changing HTML 
-// 3. Changing CSS 
-// 4. Event Listener 
+var inc = document.querySelector("#inc");
+var dec = document.querySelector('#dec');
+var h1 = document.querySelector("h1");
 
-var h1 = document.querySelector('h1');
-console.log(h1);
-h1.innerHTML = "I am Billionaire ";
-h1.style.color = "blue";
-h1.style.backgroundColor = "pink";
-h1.style.fontFamily = "Arial, sans-serif";
+var a = 0;
+inc.addEventListener("click" , function(){
+    a++;
 
-var btn = document.querySelector('button');
-btn.addEventListener('click' , function(){
-h1.innerHTML = "I am Batman!...";
-h1.style.color = "Black";
-h1.style.backgroundColor = "yellow"
-h1.style.fontSize = "40px"
+    h1.innerHTML = a ;
+    h1.style.color = " green";
 
 })
+
+dec.addEventListener("click" , function(){
+    a--;
+
+    h1.innerHTML = a ;
+    h1.style.color = " red";
+
+})
+
 
 
