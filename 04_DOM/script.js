0db144171a72b3a1f8efc257fbcfa6097570c90e
Arpit <arpit.tiwari2403@gmail.com>
2026-01-22 09:57:09 +0530

    instagram like feature

--- 04_DOM/script.js
+++ 04_DOM/script.js
@@ -1,31 +1,33 @@
-var btn = document.querySelector('button')
-var h2 = document.querySelector("h2")
-var inner = document.querySelector('.inner')
-var grow = 0 ;
+var img = document.querySelector('img')
+var love = document.querySelector("#love")
 
 
-btn.addEventListener('click',function(){
-     btn.style.pointerEvents = 'none'
 
-     var num = 50 + (Math.floor(Math.random()*50))
+img.addEventListener('dblclick',function(){
      
-
-  var zone =  setInterval(function(){
-     grow++;
-     h2.innerHTML = grow+ '%'
-     inner.style.width = grow+'%'
-
-    },num);
+    love.style.opacity = 1;
+    love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg) '
+     
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg) '
+    },800)
 
     setTimeout(function(){
-        clearInterval(zone);
-        btn.innerHTML = 'Downloaded'
-        btn.style.opacity = 0.5
-        console.log(`Downloaded in ${num/10} seconds`)
-        
-    },num*100)
+        love.style.opacity = 0;
+
+    },1000)
+
+    setTimeeout(function(){
+        love.style.transform ='translate(-50%,-50%) scale(1) rotate(-60deg) '
+     
+    },1200)
+     
+
+    });
+
+    
 
     
     
-})
+
 
