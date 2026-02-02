c17d5e26d223933ce8b1ab0fab864b27883ca2e6
Arpit <arpit.tiwari2403@gmail.com>
2026-02-02 16:25:52 +0530

    image cursor follower

--- 05_stylishCursor/script.js
+++ 05_stylishCursor/script.js
@@ -1,7 +1,37 @@
-var main = document.querySelector('.main');
-var cursor = document.querySelector('.cursor');
+// var elem = document.querySelectorAll('.elem');
+// var elemImage = document.querySelectorAll('.elem img');
 
-main.addEventListener('mousemove',function(e){
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
+// elem.forEach(function(item){
+//     item.addEventListener('mouseenter',function(){
+//     //    console.log(item.childNodes[3]) 
+//     item.childNodes[3].style.opacity = '1';
+
+//     })
+//     item.addEventListener('mouseleave',function(){
+//         item.childNodes[3].style.opacity = '0';
+//     })
+//     item.addEventListener('mousemove', function(e){
+//    elemImage.style.left = e.pageX + 'px';
+//    elemImage.style.top = e.pageY + 'px';
+// });
+// })
+var elem = document.querySelectorAll('.elem');
+
+elem.forEach(function(val){
+    console.log(val.childNodes[3])
+    val.addEventListener('mousemove',function(){
+        val.childNodes[3].style.opacity = '1';
+
+    });
+    val.addEventListener('mouseleave',function(){
+        val.childNodes[3].style.opacity = '0';
+    })
+
+    val.addEventListener('mousemove',function(dets){
+        val.childNodes[3].style.left = dets.x +'px';
+        val.childNodes[3].style.top = dets.y + 'px';
+    })
 })
+
+
+
