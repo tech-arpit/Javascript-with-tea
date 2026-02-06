374c0c07f90d5c1ea5ac051fc6cb9b7d1ed3efc7
Arpit <arpit.tiwari2403@gmail.com>
2026-02-07 04:59:33 +0530

    stoppingIntervalOnEffectCompletion

--- 07_matrixTextEffect/script.js
+++ 07_matrixTextEffect/script.js
@@ -1,7 +1,7 @@
 var para = document.querySelector('.para p');
 const character = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrstuvwxyz";
 const text = para.innerHTML
-
+let intervalId ;
 
 
 // para.addEventListener('mouseenter', function() {
@@ -26,13 +26,27 @@ para.addEventListener('mouseenter',function(){
     function randomText(){
     const str = text.split('').map((char,index)=> {
         if(index < iteration){
-            return char
+            return char;
         }
         return character.split('')[Math.floor(Math.random()*52)]
     }).join('')
     para.innerText = str ;
-    iteration += 0.25
-}
-   setInterval(randomText ,30)
+    iteration += 0.25;
+    //Stop when all letters are revealed 
+    if(iteration >= text.length){
+        clearInterval(intervalId); // Stop interval 
+    }
+} 
+// clear any existing interval first   
+if(intervalId) clearInterval(intervalId) ;
 
-})
+   intervalId = setInterval(randomText ,30);
+
+});
+
+
+//##### Leave the mouse original text will be restored########## 
+// para.addEventListener('mouseleave', function(){
+//     if(intervalId) clearInterval(intervalId);
+//     para.innerText = text;
+// })
